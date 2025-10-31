d957114a821e4a6e3915920f1a40ce38fe13f7ad
Carmen Tubbesing <ctubbesing@berkeley.edu>
2025-10-31 16:15:22 -0700

    add TRIAADS

---
+++ R-code/TRIAADS/gigafire-randig-calfire-treemap/FB_x64/FDistPowerLaw/SampleData/RunFDistPowerLaw.bat
@@ -0,0 +1,5 @@
+REM Produce a new fdist file with maximum of 10 fires per day
+..\..\bin\FDistPowerLaw foa361pl.fdist foa361_10.fdist 10
+REM Produce a new fdist file with the default 20 maximum fires per day
+..\..\bin\FDistPowerLaw foa361pl.fdist foa361_20.fdist
+
