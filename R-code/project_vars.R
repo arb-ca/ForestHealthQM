8c34a00f87ba5eb01c091c9c8e85bf883563204d
Carmen Tubbesing <ctubbesing@berkeley.edu>
2026-06-11 16:22:43 -0700

    replace temp folder with areas folder

--- R-code/project_vars.R
+++ R-code/project_vars.R
@@ -1,5 +1,5 @@
-proj_name <- "Middletown And Cobb Community Evacuation Routes - Phase 2"
-project_ID <- "25-WP-LNU-79252605"
+proj_name <- "Upper Pit River Forest Health Project"
+project_ID <- "8GG18654"
 
 
 ### Define which treatments are site prep, biomass utilization, or other treatment types that do not require FVS inputs.
@@ -7,7 +7,7 @@ noFVS <- c()
 
 ## Define which treatments get combined for FVS modeling
 ## If you only have one treatment polygon, leave this as-is
-combine_tcns <- c("5new", "6new", "7new", "8new") #example: c("1new","2new","5new")
+combine_tcns <- c(1.1, 2.1) #example: c("1new","2new","5new")
 combine_tcns_2 <- c()
 
 ### Note: Do not include reforestation treatments in `noFVS`. Even though FVS is not run on them, the FVS input files are used to determine species of regenerating conifers.
@@ -23,14 +23,15 @@ rf_shp_name <- "71625010_1_1_20250114.shp"
 
 ## Define directory with treatment shapefiles in it
 ## This can be located anywhere on your computer. It doesn't need to be in the same folder as this code
-trt_dir <- "C:/Users/ctubbesi/OneDrive - California Air Resources Board/Documents/CCI/QMs/Forest Health QM/QM Runs 2026/WPGP/Shapefiles/25_WP_LNU_79252605"
+trt_dir <- "C:/Users/ctubbesi/OneDrive - California Air Resources Board/Documents/CCI/QMs/Forest Health QM/QM Runs 2026/8GG18654/Shapefiles"
 
 
 ### Define "data" directory 
 data_dir <- "../../data"
 
 ### Define input and output directories
-temp_dir <- "../../temp"
+temp_dir <- "../../areas"
+final_polygons_dir <- "../../final_polygons"
 triaads_output_dir <- "../../triaads_input"
 output_dir_randig <- "../../randig_shapefiles"
 fvs_input_dir <- "../../FVS_Input"
